967fb1b7f3a47c3785b5f5b5c0e2fb04ce51d4b6
markmybytes <markmybytes@gmail.com>
2025-08-17 14:34:58 +0800

    Implement multi-step progress bar

--- frontend/src/views/porter/components/ProgressBar.vue
+++ frontend/src/views/porter/components/ProgressBar.vue
@@ -1,64 +1,59 @@
 <script setup lang="ts">
 import { statusBadget } from '@/definitions/styles'
 import { porter } from '@/wailsjs/go/models'
-import { computed } from 'vue'
+import ProgressNode from './ProgressNode.vue'
 
 const props = defineProps<{ progresses: porter.Progresses | null }>()
 
-const currentTask = computed(() => {
-  if (props.progresses === null || props.progresses.tasks.length == 0) {
-    return null
-  }
+// const tasks = computed(() => {
+//   if (props.progresses === null) {
+//     return []
+//   }
 
-  for (const [i, progress] of props.progresses.tasks.entries()) {
-    if (i > 0 && progress.status == 'pending') {
-      return props.progresses.tasks[i - 1]
-    }
+//   if (props.progresses.tasks.length <= 3) {
+//     return props.progresses.tasks
+//   }
 
-    if (['pending', 'running'].includes(progress.status)) {
-      return props.progresses.tasks[i]
-    }
-  }
-  return props.progresses.tasks[props.progresses.tasks.length - 1]
-})
-
-const progress = computed(() => {
-  if (currentTask.value === null) {
-    return Number.NaN
-  }
-  return Math.floor((currentTask.value.current / currentTask.value.total) * 100)
-})
+//   return []
+// })
 </script>
 
 <template>
   <div class="flex flex-col gap-y-2">
     <div class="flex justify-between">
-      <div class="flex" v-if="progresses !== null && currentTask">
-        <div class="shrink-0 w-[4.1rem]">
-          <p
-            class="inline-flex justify-center items-center max-w-[96%] h-6 px-1 rounded-sm"
-            :class="[
-              { 'animate-pulse': progresses.status.includes('ing') },
-              statusBadget[progresses.status as keyof typeof statusBadget]
-            ]"
-          >
-            <span class="text-sm truncate">{{ $t(`executeStatus.${progresses.status}`) }}</span>
-          </p>
-        </div>
-
-        <span class="font-medium">
-          {{ $t(`porter.${currentTask.name}`) }}
-        </span>
+      <div class="shrink-0 w-[4.1rem]">
+        <p
+          class="inline-flex justify-center items-center max-w-[96%] h-6 px-1 rounded-sm"
+          :class="[
+            { 'animate-pulse': progresses?.status.includes('ing') },
+            statusBadget[progresses?.status as keyof typeof statusBadget]
+          ]"
+        >
+          <span class="text-sm truncate">{{ $t(`executeStatus.${progresses?.status}`) }}</span>
+        </p>
       </div>
-
-      <span class="text-sm font-medium"> {{ Number.isNaN(progress) ? '--' : progress }} % </span>
     </div>
 
-    <div class="w-full h-2.5 bg-gray-200 rounded-full">
-      <div
-        class="h-full bg-apple-green-700 rounded-full transition-all"
-        :style="{ width: `${progress}%` }"
-      ></div>
-    </div>
+    <ol class="flex items-center w-full">
+      <ProgressNode
+        v-for="(progress, i) in props.progresses?.tasks ?? []"
+        :progress
+        :key="i"
+      ></ProgressNode>
+
+      <ProgressNode>
+        <svg
+          class="w-4 h-4 text-gray-500 lg:w-5 lg:h-5 dark:text-gray-100"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          viewBox="0 0 18 20"
+        >
+          <path
+            d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2ZM7 2h4v3H7V2Zm5.7 8.289-3.975 3.857a1 1 0 0 1-1.393 0L5.3 12.182a1.002 1.002 0 1 1 1.4-1.436l1.328 1.289 3.28-3.181a1 1 0 1 1 1.392 1.435Z"
+          />
+        </svg>
+      </ProgressNode>
+    </ol>
   </div>
 </template>
---
+++ frontend/src/views/porter/components/ProgressNode.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { statusBadget } from '@/definitions/styles'
+import { porter } from '@/wailsjs/go/models'
+
+defineProps<{ progress?: porter.Progress }>()
+</script>
+
+<template>
+  <li class="flex items-center" :class="{ grow: progress !== undefined }">
+    <span
+      class="flex items-center justify-center h-7 aspect-square rounded-full text-sm text-white"
+      :class="[
+        { 'animate-pulse': progress?.status.includes('ing') },
+        statusBadget[progress?.status as keyof typeof statusBadget] || 'bg-gray-300'
+      ]"
+    >
+      <slot>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-list-check"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0m0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0m0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"
+          />
+        </svg>
+      </slot>
+    </span>
+
+    <div class="relative flex flex-col w-full text-center" v-if="progress !== undefined">
+      <span class="text-xs absolute -top-4 w-full">{{ progress.name }}</span>
+
+      <div class="w-full h-1.5 bg-gray-200 rounded-full">
+        <div
+          class="h-full transition-all"
+          :class="[statusBadget[progress.status as keyof typeof statusBadget]]"
+          :style="{
+            width: `${progress.total === 0 ? 0 : Math.floor((progress.current / progress.total) * 100)}%`
+          }"
+        ></div>
+      </div>
+    </div>
+  </li>
+</template>
