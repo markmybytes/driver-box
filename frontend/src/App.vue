3790225b20a30f1d2857abcdabc0f73be603d6ff
markmybytes <markmybytes@gmail.com>
2025-09-05 20:59:28 +0800

    Wait until all data loaded from stoage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useAppSettingStore, useDriverGroupStore } from '@/store'
 import { AppVersion } from '@/wailsjs/go/main/App'
-import { onBeforeMount } from 'vue'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import type { RouteLocationRaw } from 'vue-router'
 import { useToast } from 'vue-toast-notification'
@@ -9,30 +9,22 @@ import { latestRelease } from './utils'
 
 const { t, locale } = useI18n()
 
-const routes: Array<{ to: RouteLocationRaw; icon: string }> = [
-  { to: '/', icon: 'fa-regular fa-house' },
-  { to: '/drivers', icon: 'fa-regular fa-file-code' },
-  { to: '/settings', icon: 'fa-solid fa-gear' },
-  { to: '/porter', icon: 'fa-solid fa-people-arrows' },
-  { to: '/app-info', icon: 'fa-solid fa-info' }
-]
-
 const $toast = useToast({ position: 'top-right' })
 
 const settingsStore = useAppSettingStore()
 
 const groupStore = useDriverGroupStore()
 
-onBeforeMount(() => {
-  groupStore.read()
+const initilisating = ref(true)
 
+Promise.all([
+  groupStore.read(),
   settingsStore
     .read()
     .then(() => {
       locale.value = settingsStore.settings.language
-
       if (settingsStore.settings.auto_check_update) {
-        AppVersion().then(version =>
+        return AppVersion().then(version =>
           latestRelease(version).then(release => {
             if (release.hasUpdate) {
               $toast.info(t('toast.updateAvailable'))
@@ -44,30 +36,62 @@ onBeforeMount(() => {
     .catch(() => {
       $toast.error(t('toast.readAppSettingFailed'))
     })
-})
+]).finally(() => (initilisating.value = false))
+
+const routes: Array<{ to: RouteLocationRaw; icon: string }> = [
+  { to: '/', icon: 'fa-regular fa-house' },
+  { to: '/drivers', icon: 'fa-regular fa-file-code' },
+  { to: '/settings', icon: 'fa-solid fa-gear' },
+  { to: '/porter', icon: 'fa-solid fa-people-arrows' },
+  { to: '/app-info', icon: 'fa-solid fa-info' }
+]
 </script>
 
 <template>
   <div class="flex h-screen">
-    <aside class="w-12">
-      <div class="flex justify-center h-full bg-gray-50">
-        <ul class="mt-6 space-y-3 font-medium">
-          <li v-for="(link, i) in routes" :key="i">
-            <RouterLink
-              :to="link.to"
-              class="flex p-2 rounded-lg hover:bg-gray-200"
-              activeClass="text-apple-green-900 bg-powder-blue-400"
-              draggable="false"
-            >
-              <font-awesome-icon :icon="link.icon" />
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-    </aside>
+    <Transition name="fade" mode="out-in">
+      <template v-if="!initilisating">
+        <div class="flex w-full">
+          <aside class="w-12">
+            <div class="flex justify-center h-full bg-gray-50">
+              <ul class="mt-6 space-y-3 font-medium">
+                <li v-for="(link, i) in routes" :key="i">
+                  <RouterLink
+                    :to="link.to"
+                    class="flex p-2 rounded-lg hover:bg-gray-200"
+                    activeClass="text-apple-green-900 bg-powder-blue-400"
+                    draggable="false"
+                  >
+                    <font-awesome-icon :icon="link.icon" />
+                  </RouterLink>
+                </li>
+              </ul>
+            </div>
+          </aside>
 
-    <main class="w-full h-full p-3">
-      <RouterView></RouterView>
-    </main>
+          <main class="w-full h-full p-3">
+            <RouterView></RouterView>
+          </main>
+        </div>
+      </template>
+
+      <template v-else>
+        <div class="m-auto">
+          <span class="loading loading-dots loading-xl"></span>
+        </div>
+      </template>
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
